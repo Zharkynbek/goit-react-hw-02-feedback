2d09c18d1276902c3de92ef1ca23bdd2ed59beda
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-06-02 19:20:42 +0200

    add first task(no completed)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  goodFeedback = () => {
+    this.setState((prevState) => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+
+  neutralFeedback = () => {
+    this.setState((prevState) => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+
+  badFeedback = () => {
+    this.setState((prevState) => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  // =======================================
+
+  // countTotalFeedback() {
+  //   return this.state.good + this.state.neutral + this.state.bad;
+  // }
+
+  // =======================================
+
+  // countTotalFeedback() {
+  //   this.setState((prevState) => {
+  //     return {
+  //       good: prevState.bad + 1,
+  //       neutral: prevState.bad + 1,
+  //       bad: prevState.bad + 1,
+  //     };
+  //   });
+  // }
+
+  // ======================================
+  // countTotalFeedback = () => {
+  //   this.setState((prevState) => {
+  //     return {
+  //       good: prevState.good + 1,
+  //     };
+  //   });
+
+  //   this.setState((prevState) => {
+  //     return {
+  //       neutral: prevState.neutral + 1,
+  //     };
+  //   });
+
+  //   this.setState((prevState) => {
+  //     return {
+  //       bad: prevState.bad + 1,
+  //     };
+  //   });
+  // };
+
+  render() {
+    // const {feedback}  = this.state;
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+
+        <div className="Counter">
+          <button type="button" onClick={this.goodFeedback}>
+            Good
+          </button>
+
+          <button type="button" onClick={this.neutralFeedback}>
+            Neutral
+          </button>
+
+          <button type="button" onClick={this.badFeedback}>
+            Bad
+          </button>
+        </div>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total: {this.state.good + this.state.neutral + this.state.bad}</p>
+        <span>
+          <p>Positive feedback:</p>{" "}
+        </span>
+      </div>
+    );
+  }
 }
 
 export default App;
